c84688b1fe630d041fb17d4f4a0fd5aca53d0eca
Natasha Jones <tasha.k.jones@gmail.com>
2016-07-27 12:53:18 -0400

    set up directory

---
+++ upcoming.rb
@@ -0,0 +1,9 @@
+require 'net/http'
+
+class UpcomingBills
+
+  def fetch
+  end
+
+  private
+end
---
+++ upcoming_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe Upcoming do
+  describe '#fetch_upcoming_bills' do
+
+    it 'returns the next 3 bills' do
+    end
+    
+  end
+end
